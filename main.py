eedc735d3df0817e647c69785730e511cbfeb2d9
Pratyush Sharma <pratoabd123@gmail.com>
2021-08-19 16:48:46 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import pyqrcode 
+from pyqrcode import QRCode 
+   
+s = "https://www.youtube.com/channel/UCeO9hPCfRzqb2yTuAn713Mg"
+url = pyqrcode.create(s)   
+url.svg("myyoutube.svg", scale = 8) 
